ec1fb2442a2e377c063aed7f401faf6a1b9e9954
Oliver <oliver@MSI>
2024-08-30 14:59:23 -0700

    adoption center seed

--- pixelpets-seed.sql
+++ pixelpets-seed.sql
@@ -7,7 +7,7 @@ VALUES
         'user1@email.com',
         NOW(),
         NOW()),
-       ('user2',
+        ('user2',
         '$2b$12$AZH7virni5jlTTiGgEg4zu3lSvAw68qVEfSIOjJ3RqtbJbdW/Oi5q',
         'user2@email.com',
         NOW(),
@@ -15,9 +15,13 @@ VALUES
 
 INSERT INTO pets (owner_id, name, species, color, gender, happiness, hunger, last_played, last_fed, created_at, updated_at)
 VALUES 
-    ((SELECT id FROM users WHERE id = 1), 
-        'Fluffy', 'Cat', 'White', 'f', 100, 100, NULL, NULL, NOW(), NOW()),
-    ((SELECT id FROM users WHERE id = 2), 
-        'Buddy', 'Dog', 'Brown', 'm', 50, 50, NOW(), NOW(), NOW(), NOW()),
-    ((SELECT id FROM users WHERE id = 2), 
-        'Goldie', 'Fish', 'Gold', 'f', 70, 50, NOW(), NOW(), NOW(), NOW());
+        ((SELECT id FROM users WHERE id = 1), 'Fluffy', 'Cat', 'White', 'f', 100, 100, NULL, NULL, NOW(), NOW()),
+        ((SELECT id FROM users WHERE id = 2), 'Buddy', 'Dog', 'Brown', 'm', 50, 50, NOW(), NOW(), NOW(), NOW()),
+        ((SELECT id FROM users WHERE id = 2), 'Goldie', 'Fish', 'Gold', 'f', 70, 50, NOW(), NOW(), NOW(), NOW()),
+        (NULL, 'Patches', 'Dog', 'grey', 'm', 80, 30, NOW(), NOW(), NOW(), NOW()), 
+        (NULL, 'Whiskers', 'Cat', 'black', 'f', 90, 60, NOW(), NOW(), NOW(), NOW());
+
+INSERT INTO adoption_center (pet_id, posted_at) 
+VALUES 
+        (4, NOW()), 
+        (5, NOW());
